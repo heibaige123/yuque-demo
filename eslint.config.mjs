f4abc94cbc4ed99ef56b8ae76d5af6bd29e83539
隔壁的猫先生 <58593878+heibaige123@users.noreply.github.com>
2024-06-30 23:36:52 +0800

    feature:solid.js项目初始化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,13 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   solid: true,
+
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
 })
