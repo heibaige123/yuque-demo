561953b5ef00a2e8e8e9be0162e796d2e6020093
隔壁的猫先生 <58593878+heibaige123@users.noreply.github.com>
2024-08-13 22:33:02 +0800

    👌 IMPROVE: 优化shadcn-ui 样式类编译

--- packages/note-editor/src/renderer/src/main.tsx
+++ packages/note-editor/src/renderer/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'uno.css';
 import 'virtual:uno.css';
 import 'virtual:unocss-devtools';
 import './assets/style/index.css';
 import App from './App';
-import '@huo-note/shadcn-ui/dist/style.css';
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLDivElement).render(
 	<React.StrictMode>
